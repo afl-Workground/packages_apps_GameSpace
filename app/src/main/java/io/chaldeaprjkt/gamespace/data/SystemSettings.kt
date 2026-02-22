9775d3c111126ab68a490a0e7023e5d0864a9584
Romeo <rbesar210311@gmail.com>
2026-02-22 06:10:30 +0000

    GameSpace: CrDroid -> afterlife
    Change-Id: If1ed1ccfb13543d3d4b269fbf90e310855d7b754
    Signed-off-by: Romeo <rbesar210311@gmail.com>

--- app/src/main/java/io/chaldeaprjkt/gamespace/data/SystemSettings.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/data/SystemSettings.kt
@@ -22,8 +22,6 @@ import android.provider.Settings
 import io.chaldeaprjkt.gamespace.utils.GameModeUtils
 import javax.inject.Inject
 
-import lineageos.providers.LineageSettings
-
 class SystemSettings @Inject constructor(
     context: Context,
     private val gameModeUtils: GameModeUtils
@@ -61,13 +59,13 @@ class SystemSettings @Inject constructor(
         }
 
     var threeScreenshot
-        get() = LineageSettings.System.getIntForUser(
-            resolver, LineageSettings.System.KEY_THREE_FINGERS_SWIPE_ACTION, 0,
+        get() = Settings.System.getIntForUser(
+            resolver, Settings.System.SWIPE_TO_SCREENSHOT, 0,
             UserHandle.USER_CURRENT
         )
         set(value) {
-            LineageSettings.System.putIntForUser(
-                resolver, LineageSettings.System.KEY_THREE_FINGERS_SWIPE_ACTION,
+            Settings.System.putIntForUser(
+                resolver, Settings.System.SWIPE_TO_SCREENSHOT,
                 value, UserHandle.USER_CURRENT
             )
         }
